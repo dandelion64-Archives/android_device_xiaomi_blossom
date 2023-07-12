60e17df73c5acd082e8e5c3c5fc5c40506136220
Sushrut1101 <guptasushrut@gmail.com>
2023-07-12 20:28:25 +0800

    blossom: import USB/ADB props from dump
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
 
+# HACK: Avoid usb crash
+PRODUCT_PRODUCT_PROPERTIES := \
+    persist.adb.nonblocking_ffs=0 \
+    ro.adb.nonblocking_ffs=0
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6765 \
